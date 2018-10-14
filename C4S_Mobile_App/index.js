b76271e6d642e6f930b4edcb26f129d941e90848
Arnlee Vizcayno <vizcayno_arnlee@yahoo.com>
2018-10-14 10:09:34 +0800

    Splash screen transition to Login screen.

--- C4S_Mobile_App/index.js
+++ C4S_Mobile_App/index.js
@@ -1,9 +1,25 @@
 /** @format */
 
+import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import Login from './Login';
 import Splash from './Splash';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Login);
+class Main extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { currentScreen: 'Splash'};
+		setTimeout(()=>{
+			this.setState({ currentScreen: 'Login' })
+		}, 2000)
+	}
+	render() {
+		const { currentScreen } = this.state
+		let mainScreen = currentScreen === 'Splash' ? <Splash /> : <Login />
+		return mainScreen
+	}
+}
+
+AppRegistry.registerComponent(appName, () => Main);
