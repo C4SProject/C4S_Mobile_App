d2ec04d368073e3c15a8206ee93c81e2d89dbcb1
Arnlee Vizcayno <vizcayno_arnlee@yahoo.com>
2018-12-27 16:59:15 +0800

    Register and Forgot password view.

--- C4S/Sell/Sell/LoginViewController.m
+++ C4S/Sell/Sell/LoginViewController.m
@@ -19,6 +19,16 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [self.navigationController setNavigationBarHidden:YES animated:animated];
+    [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [self.navigationController setNavigationBarHidden:NO animated:animated];
+    [super viewWillDisappear:animated];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
