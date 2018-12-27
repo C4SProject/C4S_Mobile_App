8a19baeb5777365148cb2c16795dcd3ab7bf5a83
Arnlee Vizcayno <vizcayno_arnlee@yahoo.com>
2018-12-27 16:20:33 +0800

    Splash screen and Login Screen

---
+++ C4S/Sell/Sell/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Sell
+//
+//  Created by Arnlee Vizcayno on 27/12/2018.
+//  Copyright © 2018 Arnlee Vizcayno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
